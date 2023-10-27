8b6704f818ec44863bbe08ff9b10803b19d4594e
Hieu Nguyen <hieu.nguyen@ncsgroup.vn>
2023-10-27 08:47:46 +0700

    add product.html file

---
+++ shopsport-parent/shopsport-server/src/main/java/com/shopsport/server/controller/ProductController.java
@@ -0,0 +1,13 @@
+package com.shopsport.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+
+  @GetMapping("/products")
+  public String viewPage() {
+    return "products";
+  }
+}
