afcd8ddb39fba662ebc92e27ea49b25ada252239
= <=>
2023-11-14 21:16:18 +0700

    hihi

--- shopsport-parent/shopsport-client/src/main/java/com/shopsport/client/controller/OrderController.java
+++ shopsport-parent/shopsport-client/src/main/java/com/shopsport/client/controller/OrderController.java
@@ -1,13 +1,20 @@
 package com.shopsport.client.controller;
 
+import com.shopsport.client.service.CartItemService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class OrderController {
 
+  private final CartItemService cartItemService;
+
   @GetMapping("/checkout")
-  public String checkoutView() {
+  public String checkoutView(Model model) {
+    model.addAttribute("products", cartItemService.listByCustomer());
     return "checkout";
   }
 }
--- shopsport-parent/shopsport-client/src/main/java/com/shopsport/client/repository/CartItemRepository.java
+++ shopsport-parent/shopsport-client/src/main/java/com/shopsport/client/repository/CartItemRepository.java
@@ -1,7 +1,6 @@
 package com.shopsport.client.repository;
 
 import com.shopsport.common.entity.CartItem;
-import com.shopsport.common.entity.Customer;
 import com.shopsport.common.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,6 +12,6 @@ public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
   @Query(value = "select count(*) from CartItem ci where ci.customer.id = :customerId")
   Integer getNumberOfProduct(Integer customerId);
 
-  @Query(value = "select ci.product  from CartItem ci")
+  @Query(value = "select ci.product from CartItem ci where ci.customer.id = :customerId")
   List<Product> findByCustomer(Integer customerId);
 }
